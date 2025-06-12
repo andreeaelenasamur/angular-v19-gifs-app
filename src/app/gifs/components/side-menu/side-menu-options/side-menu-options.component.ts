3db0417fdaa94d8dae73cd52f08778fafed19e61
andreeaelenasamur <andelenasamur@gmail.com>
2025-06-12 13:54:53 +0200

    Add font awesome library and make changes in side menu options component

--- src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -1,8 +1,36 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption {
+  icon: string;
+  label: string;
+  router: string;
+  subLabel: string;
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './side-menu-options.component.html'
 })
-export class SideMenuOptionsComponent { }
+export class SideMenuOptionsComponent {
+
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs Populares',
+      router: '/dashboard/trending',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      subLabel: 'Buscar gifs',
+      router: '/dashboard/search',
+    },
+  ]
+
+}
